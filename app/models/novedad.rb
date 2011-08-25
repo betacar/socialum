d1edfd27564a513230218cc4799257eb47757e73
Antonio Bastardo <BAUXILUM\antonio.bastardo@ba-2027l.bauxilum.com.ve>
2011-08-25 11:33:05 -0430

    1. En el modelo de DescargaBauxita se cambio el método de salvar la novedad para que llame a un método común denominado create en el modelo Novedad, ademas se agrego un metodo llamado datos a la novedad para devolver un hash con los datos a listar. 2. Se ajusto la vista donde se muestran las gabarras para poder procesar el hash de los valores devueltos

--- app/models/novedad.rb
+++ app/models/novedad.rb
@@ -4,4 +4,26 @@ class Novedad < ActiveRecord::Base
   belongs_to :tipo_novedad
   stampable
   attr_accessor :login, :inicio_evento, :fin_evento
+  def self.create(proceso,desc_novedad,inicio_novedad,fin_novedad)
+      novedad = self.new
+      novedad.proceso = proceso
+      novedad.inicio_novedad = DateTime.strptime(inicio_novedad, '%d/%m/%Y %H:%M').to_datetime.to_s(:db) unless inicio_novedad.empty?
+      novedad.fin_novedad = DateTime.strptime(fin_novedad, '%d/%m/%Y %H:%M').to_datetime.to_s(:db) unless fin_novedad.empty?
+      novedad.desc_novedad = desc_novedad
+      if novedad.save
+        novedad
+      else
+        raise Exceptions::PresenciaValoresExcepcion.new('Fallo al intentar salvar la Novedad')
+      end
+  end
+  def datos
+      evento = Hash.new
+      evento[:login] = Empleado.find(self.creator_id)
+      evento[:login] = (evento[:login].nombres + ' ' + evento[:login].apellidos).titleize
+      evento[:inicio] = self.inicio_novedad.to_s(:dia_mes)
+      evento[:fin] = self.fin_novedad.to_s(:dia_mes)
+      evento[:descipcion] = self.desc_novedad
+      evento
+  end
 end
+
