61d153774711abf83fc374b845f49da9be1dace4
Carlos Betancourt <carlos.betancourt@bauxilum.com.ve>
2011-09-05 19:43:00 -0430

    Se eliminaron los modelos de ArriboBuque y Ensayo de laboratorios, y sus dependencias, por no ser necesario en el proyecto (en el caso del primero) y por la fusión de la vista de BD de BaxGabarras y Análisis de laboratorio.

--- app/models/arribo_buque.rb
+++
@@ -1,6 +0,0 @@
-class ArriboBuque < ActiveRecord::Base
-  set_table_name 'arribos_buques'
-  belongs_to :tipo_materia
-  has_many :DescargaBauxitas, :as => :arribo
-  stampable
-end
--- test/unit/ensayo_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class EnsayoTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
