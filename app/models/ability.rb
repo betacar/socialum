1f06006d0ece36450eecccdd53fbff2723d37ba6
Carlos Betancourt <carlos.betancourt@bauxilum.com.ve>
2011-09-19 09:54:45 -0430

    Modificación a los abilitys de CanCan, para operar las descargas de material y la cola de buques de materiales.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,17 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user
 
-    if user.role? :super_admin
-        can :manage, :all
-    elsif user.role? :admin_descargas
-        can :manage, [ArriboBauxita,ArriboBuque,DescargaBauxita]
-        can :read, User
+    if user.role? :admin # Puede administrar todo
+      can :manage, :all
+    elsif user.role? :op_descargas # Puede operar los arribos y descargas de BAX y Buques
+      can :manage, [ArriboBauxita, DescargaBauxita, DescargaOtro]
+      can [:reportar, :descargar], Buque
+      can :read, User
+    elsif user.role? :op_buque # Puede operar la cola de buques que arribarán con material
+      can [:new, :create, :edit, :update, :destroy], Buque
+      can :read, User
     elsif user.role? :usuario_logueado
-        can :read, :all
+      can :read, :all
     else
       can :read, :all
     end
