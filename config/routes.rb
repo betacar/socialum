90d1b37a48935230261ab76054bbccd1a3dda1cf
Carlos Betancourt <carlos.betancourt@bauxilum.com.ve>
2012-09-03 17:02:28 -0430

    New RESTful routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,33 @@
 Socialum::Application.routes.draw do
-  root :to => 'overview#index'
-  resources :transportes
-  resources :tipo_transportes
-  resources :subprocesos
-  resources :procesos
-  resources :buques
-  resources :equipos
-  resources :alarmas
-  resources :stock_gabarras
-  resources :estatus_gabarras
-  resources :locacions
-  resources :tipo_materias
-  resources :arribos
-  resources :descargar
+  # Raiz de la app
+  root :to => 'baxs#index'
+
+  # Recursos catalogo
   resources :novedades
 
-  # Devise stuff
-  # match 'users' => '#index', :as => :devise_for
+  # Recursos anidados: arribos y descargas
+  resources :baxs, :only => [:index, :show] do
+    resources :bax_gabarras, :only => [:index]
+    get '/bax_gabarras/:gabarra_id' => 'bax_gabarras#show', 
+      :constraints => { :gabarra_id => /[A-Z]{2,4}-\d{2,4}/ }, 
+      :as => :bax_gabarra
 
-  # match 'users' => '#index', 
-  #       :as => :devise_for, 
-  #       :path_names => { :sign_in => 'login', :sign_out => 'logout' }
+    resources :arribos_bauxitas, :except => [:new, :edit] do
+      resources :descargas, :except => [:new, :edit] do
+        resources :novedades
+      end
+    end
+  end
 
-  # match '/overview' => 'overview#index', 
-  #       :as => :user_root
+  # Recursos de autenticacion
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get 'login' => 'devise/sessions#new', :as => :new_user_session
+    post 'login' => 'devise/sessions#create', :as => :user_session
+    match 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session,
+      :via => Devise.mappings[:user].sign_out_via
+  end
 
-  devise_for :users
-
-  # as :user do
-  #   get "login" => "devise/session#new", :as => :new_user_session
-  #   post 'login' => 'devise/sessions#create', :as => :user_session
-  #   delete "logout" => "devise/sessions#destroy", :as => :destroy_user_session
-  # end
-
+  # Plantilla de recursos
   match '/:controller(/:action(/:id))'
-
-  match 'arribos/:action/:num_zarpe/:anio_zarpe' => 'arribos#reportar', 
-        :num_zarpe => /\d{3}/, 
-        :anio_zarpe => /\d{4}/
-        
-  match 'arribos/:action/:num_zarpe/:anio_zarpe' => 'arribos#gabarras', 
-        :num_zarpe => /\d{3}/, 
-        :anio_zarpe => /\d{4}/
-        
-  match 'arribos/:action/:num_zarpe/:anio_zarpe/:gabarra_id' => 'arribos#gabarra', 
-        :as => :arribos_gabarra, 
-        :gabarra_id => /(\w{2,5})-(\d{3,4}|\w{4})/, 
-        :num_zarpe => /\d{3}/, 
-        :anio_zarpe => /\d{4}/
-        
-  match 'descargar/:action/:num_zarpe/:anio_zarpe/:gabarra_id(.:format)' => 'descargar#gabarra', 
-        :gabarra_id => /(\w{2,5})-(\d{3,4}|\w{4})/, 
-        :num_zarpe => /\d{3}/, 
-        :anio_zarpe => /\d{4}/
-        
-  match 'descargar/:action/:tipo_materia_id/:buque_id' => 'descargar#buque', 
-        :tipo_materia_id => /\d{1,}/, 
-        :buque_id => /\d{1,}/
 end
