bfd921d4e97d1c2286b6da8c967f7696b9f725bc
Carlos Betancourt <carlos.betancourt@bauxilum.com.ve>
2012-09-16 17:10:30 -0430

    Deleted RSpec test files.

--- spec/models/empresa_maritima_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe EmpresaMaritima do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/empresa_transporte/index.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "empresa_transporte/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
