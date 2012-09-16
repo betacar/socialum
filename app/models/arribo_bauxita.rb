153c4becd343c4fff36f28529121d4161b69000b
Carlos Betancourt <carlos.betancourt@bauxilum.com.ve>
2012-09-16 17:06:57 -0430

    Updated model validatios: format, presence, uniqueness, and others.

--- app/models/arribo_bauxita.rb
+++ app/models/arribo_bauxita.rb
@@ -9,5 +9,12 @@ class ArriboBauxita < ActiveRecord::Base
 
   attr_accessible :bax_id, :fecha_hora_arribo_bauxita, :descargado
 
-  validates_uniqueness_of :bax_id, :message => 'El BAX ya ha sido reportado.'
+  validates :bax_id, :fecha_hora_arribo_bauxita, :presence => true
+  validates_uniqueness_of :bax_id, 
+                          :message => 'El BAX ya ha sido reportado.'
+  validates :bax_id, 
+            :format => { :with => /\d{3}-\d{4}/, 
+                         :message => 'El BAX debe cumplir el formato establecido.' }
+  validates :fecha_hora_arribo_bauxita, 
+            :date => { :after => Proc.new { Time.now } }
 end
