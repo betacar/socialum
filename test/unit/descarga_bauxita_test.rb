e2cc033c6a936fc9d7405870cf5b677f6bd726d6
Carlos Betancourt <carlos.betancourt@bauxilum.com.ve>
2012-09-17 15:57:08 -0430

    Unload should not be empty.

--- test/unit/descarga_bauxita_test.rb
+++ test/unit/descarga_bauxita_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class DescargaBauxitaTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test 'unloads should not be blank' do
+    descarga = DescargaBauxita.new
+    assert !descarga.save, 'La descarga es vacia'
   end
 end
