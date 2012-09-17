bac7958f313c230809989c8f0f3950592e079e5c
Carlos Betancourt <carlos.betancourt@bauxilum.com.ve>
2012-09-17 15:37:19 -0430

    - Added a percetage attribute and tooltip to the progress bar. - Added a helper method to calculate the unloads progress percentage.

--- app/helpers/baxs_helper.rb
+++ app/helpers/baxs_helper.rb
@@ -2,4 +2,9 @@ module BaxsHelper
   def formato_bax bax_id
     bax_id.gsub(/-/, '/')
   end
+
+  def avance bax
+    progreso = bax.arribo_bauxita.descarga_bauxitas.progreso * 100 / bax.carga_transportar
+    number_to_percentage(progreso, :precision => 2)
+  end
 end
