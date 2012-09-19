187e65bb220f28d44228e0064af412bd4901ffc1
Carlos Betancourt <carlos.betancourt@bauxilum.com.ve>
2012-09-19 16:36:17 -0430

    Validate presence and association.

--- app/models/equipo.rb
+++ app/models/equipo.rb
@@ -1,4 +1,10 @@
 class Equipo < ActiveRecord::Base
-  belongs_to :tipo_equipo
+  belongs_to :tipos_equipo
   stampable
+
+  attr_accessible :nombre_equipo, :tipo_equipo_id
+
+  validates_presence_of :nombre_equipo, :message => 'Debe indicar el nombre del equipo'
+  validates_presence_of :tipo_equipo_id, :message => 'Debe indicar el tipo de equipo'
+  validates_associated :tipos_equipo
 end
